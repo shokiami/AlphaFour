17437d327f38f9c123441b683942d95b12c7a956
shokiami <kiami.sho@gmail.com>
2023-07-09 00:11:54 -0700

    fixed git mispush

---
+++ train.py
@@ -0,0 +1,15 @@
+from ai import AI
+import numpy as np
+
+state = np.array([
+  [ 0,  0,  0,  0,  0,  0,  0],
+  [ 0,  0,  0,  0,  0,  0,  0],
+  [ 0,  0,  0,  0,  0,  0,  0],
+  [ 0,  0,  1,  0,  0,  0,  0],
+  [ 1, -1,  1,  0,  0,  0,  0],
+  [-1, -1, -1,  0,  1,  0,  0],
+])
+
+ai = AI()
+ai.learn()
+print(ai.compute(state))
