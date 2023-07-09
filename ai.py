5773302f4d6841d9bccdcd158f596fffb69f8ccd
shokiami <kiami.sho@gmail.com>
2023-07-08 23:20:20 -0700

    more progress ig

--- ai.py
+++ ai.py
@@ -8,63 +8,56 @@ import os
 import csv
 import matplotlib.pyplot as plt
 
-LEARNING_RATE = 0.0001
-EPSILON = 0.1
-GAMMA = 0.9
-WIN_REWARD = 10.0
-LIVING_REWARD = 0.1
+NUM_HIDDEN = 4
+NUM_CHANNELS = 64
 
-MODEL = 'qnet.pt'
-LOSSES_CSV = 'losses.csv'
-LOSSES_PLOT = 'losses.png'
 
-UCB_C = 1.41421356237  # sqrt(2)
-MCTS_ROLLOUTS = 1000
+LEARNING_RATE = 0.001
+WEIGHT_DECAY = 0.0001
+
+MODEL_PATH = 'model.pt'
+# LOSSES_CSV = 'losses.csv'
+# LOSSES_PLOT = 'losses.png'
+
+UCB_C = 2.0
+MCTS_ROLLOUTS = 10
+
+NUM_ITRS = 2
+GAMES_PER_ITR = 10
+NUM_EPOCHS = 8
+BATCH_SIZE = 4
+
+NUM_PARALLEL_GAMES = 100
 
 class MCTSNode:
-  def __init__(self, state, parent=None, prev_action=None):
+  def __init__(self, state, parent=None, prev_action=None, prior=0):
     self.state = state
     self.parent = parent
     self.prev_action = prev_action
+    self.prior = prior
     self.children = []
-    self.expandable_actions = get_valid_actions(state)
     self.visit_count = 0
     self.value_sum = 0
-
-  def is_fully_expanded(self):
-    return not np.any(self.expandable_actions) and len(self.children) > 0
 
   def select(self):
     return max(self.children, key=lambda c: c.ucb())
 
   def ucb(self):
     q_val = 0.0 if self.visit_count == 0 else 1.0 - (self.value_sum / self.visit_count + 1.0) / 2.0
-    return q_val + UCB_C * np.sqrt(np.log(self.parent.visit_count) / self.visit_count)
+    return q_val + UCB_C * self.prior * np.sqrt(self.parent.visit_count / (self.visit_count + 1.0))
 
-  def expand(self):
-    action = np.random.choice(np.where(self.expandable_actions)[0])
-    self.expandable_actions[action] = 0
-    next_state = -get_next_state(self.state, 1, action)
-    child = MCTSNode(next_state, self, action)
-    self.children.append(child)
-    return child
+  def expand(self, policy):
+    for action, prob in enumerate(policy):
+      if prob > 0:
+        next_state = -get_next_state(self.state, 1, action)
+        child = MCTSNode(next_state, self, action, prob)
+        self.children.append(child)
 
-  def simulate(self):
-    player = 1
-    rollout_state = self.state
-    while True:
-      action = np.random.choice(np.where(get_valid_actions(rollout_state))[0])
-      rollout_state = get_next_state(rollout_state, player, action)
-      terminal, win = is_terminal(rollout_state, action)
-      if terminal:
-        return player if win else 0.0
-      player = -player
-
-  def backprop(self, value):
+  def backpropagate(self, value):
     self.value_sum += value
     self.visit_count += 1
     if self.parent is not None:
-      self.parent.backprop(-value)
+      self.parent.backpropagate(-value)
 
 class ResBlock(nn.Module):
   def __init__(self, in_channels, out_channels):
@@ -85,11 +78,11 @@ class ResBlock(nn.Module):
     x = F.relu(x)
     return x
 
-class QNet(nn.Module):
+class ResNet(nn.Module):
   def __init__(self, num_blocks, num_channels):
-    super(QNet, self).__init__()
+    super(ResNet, self).__init__()
     self.start_block = nn.Sequential(
-      nn.Conv2d(2, num_channels, kernel_size=3, padding=1),
+      nn.Conv2d(3, num_channels, kernel_size=3, padding=1),
       nn.BatchNorm2d(num_channels),
       nn.ReLU()
     )
@@ -120,133 +113,91 @@ class QNet(nn.Module):
 
 class AI:
   def __init__(self):
-    if os.path.isfile(MODEL):
-      self.qnet = torch.load(MODEL)
-    else:
-      self.qnet = QNet(9, 128)
+    self.model = ResNet(NUM_HIDDEN, NUM_CHANNELS)
+    if os.path.isfile(MODEL_PATH):
+      self.model.load_state_dict(torch.load(MODEL_PATH))
+    self.optimizer = torch.optim.Adam(self.model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+
+  def to_tensor(self, states):
+    return torch.tensor(np.stack((states == 1, states == 0, states == -1)).swapaxes(0, 1), dtype=torch.float32)
+
+  def predict(self, state):
+    with torch.no_grad():
+      policy, value = self.model(self.to_tensor(np.expand_dims(state, 0)))
+    policy = torch.softmax(policy, axis=1).squeeze(0).numpy()
+    policy[np.invert(get_valid_actions(state))] = 0.0
+    policy /= np.sum(policy)
+    value = value.item()
+    return policy, value
 
   def mcts_search(self, state):
-    root = MCTSNode(state)
+    self.model.eval()
+    root = MCTSNode(state.copy())
     for i in range(MCTS_ROLLOUTS):
       node = root
-      while node.is_fully_expanded():
+      while len(node.children) > 0:
         node = node.select()
       terminal, win = is_terminal(node.state, node.prev_action)
       if terminal:
         value = -1.0 if win else 0.0
       else:
-        node = node.expand()
-        value = node.simulate()
-      node.backprop(value)
+        policy, value = self.predict(node.state)
+        node.expand(policy)
+      node.backpropagate(value)
     action_probs = np.zeros(ACTION_SPACE)
     for child in root.children:
       action_probs[child.prev_action] = child.visit_count
     action_probs /= np.sum(action_probs)
     return action_probs
 
-  def board_to_state(self, board):
-    mat = board.mat.copy()
-    mat[mat == 2] = -1.0
-    if board.player == 2:
-      mat *= -1.0
-    return torch.FloatTensor(mat).unsqueeze(0)
+  def self_play(self):
+    examples = []
+    player = 1
+    state = init_state()
+    while True:
+      action_probs = self.mcts_search(player * state) ** (1 / 1.25)
+      action_probs /= action_probs.sum()
+      action = np.random.choice(7, p=action_probs)
+      state = get_next_state(state, player, action)
+      examples.append([player * state, action_probs, 0.0])
+      terminal, win = is_terminal(state, action)
+      if terminal:
+        if win:
+          for i in range(len(examples)):
+            examples[i][2] = 1.0 if (len(examples) - i) % 2 == 1 else -1.0
+        return examples
+      player = -player
 
-  def compute_move(self, board):
-    return np.random.randint(7), 0
-    # state = self.board_to_state(board)
-    # with torch.no_grad():
-    #   qvals = self.qnet(state).numpy()
-    # action = self.safe_argmax(qvals, board)
-    # qval = qvals[action]
-    # return action, qval
+  def train(self, examples):
+    self.model.train()
+    np.random.shuffle(examples)
+    for i in range(0, len(examples), BATCH_SIZE):
+      states, action_probs, values = zip(*examples[i:min(i + BATCH_SIZE, len(examples))])
+      states = self.to_tensor(np.array(states))
+      action_probs = torch.tensor(np.array(action_probs), dtype=torch.float32)
+      values = torch.tensor(np.array(values), dtype=torch.float32).unsqueeze(1)
+      pred_policies, pred_values = self.model(states)
+      # print(pred_policies, action_probs)
+      # print(pred_values, values)
+      loss = F.cross_entropy(pred_policies, action_probs) + F.mse_loss(pred_values, values)
+      self.optimizer.zero_grad()
+      loss.backward()
+      self.optimizer.step()
+      print(loss.item())
 
-  def safe_argmax(self, qvals, board):
-    for action in np.argsort(qvals):
-      if board.placeable(action):
-        return action
+  def learn(self):
+    for i in range(NUM_ITRS):
+      examples = []
+      for j in range(GAMES_PER_ITR):
+        examples += self.self_play()
+        print(j)
+      for epoch in range(NUM_EPOCHS):
+        self.train(examples)
+        print(epoch)
+      # torch.save(self.model.state_dict(), MODEL_PATH)
+      print(i)
 
-  def random_move(self, board):
-    action = np.random.randint(7)
-    while not board.placeable(action):
-      action = np.random.randint(7)
-    return action
-
-  def update(self, state, action, reward, optimizer):
-    qvals = self.qnet(state)
-    new_qvals = qvals.detach().clone()
-    new_qvals[action] = reward
-    optimizer.zero_grad()
-    loss = F.mse_loss(qvals, new_qvals)
-    loss.backward()
-    optimizer.step()
-    return loss.item()
-
-  def train(self):
-    game = 1
-    if os.path.isfile(LOSSES_CSV):
-      with open(LOSSES_CSV, 'r') as losses_csv:
-        for row in losses_csv:
-          game += 1
-
-    optimizer = optim.Adam(self.qnet.parameters(), LEARNING_RATE)
-
-    with open(LOSSES_CSV, 'a') as losses_csv:
-      loss_writer = csv.writer(losses_csv)
-
-      while True:
-        board = Board()
-        losses = []
-
-        state_p = None
-        action_p = None
-        state_pp = None
-        action_pp = None
-
-        while board.winner == 0:
-          state = self.board_to_state(board)
-          with torch.no_grad():
-            qvals = self.qnet(state).numpy()
-
-          action = self.safe_argmax(qvals, board)
-
-          if action_pp:
-            reward = LIVING_REWARD + GAMMA * qvals[action]
-            losses.append(self.update(state_pp, action_pp, reward, optimizer))
-
-          if np.random.rand() < EPSILON:
-            action = self.random_move(board)
-
-          board.place(action)
-
-          state_pp = state_p
-          action_pp = action_p
-          state_p = state
-          action_p = action
-
-        if board.winner == 3:
-          losses.append(self.update(state_p, action_p, 0.0, optimizer))
-          losses.append(self.update(state_pp, action_pp, 0.0, optimizer))
-        else:
-          losses.append(self.update(state_p, action_p, WIN_REWARD, optimizer))
-          losses.append(self.update(state_pp, action_pp, -WIN_REWARD, optimizer))
-
-        loss = np.mean(losses)
-        loss_writer.writerow([loss])
-        torch.save(self.qnet, MODEL)
-        print(f'game: {game}, winner: {int(board.winner)}, loss: {loss}')
-        game += 1
-
-  def plot(self):
-    losses = []
-    if os.path.isfile(LOSSES_CSV):
-      with open(LOSSES_CSV, 'r') as losses_csv:
-        for row in losses_csv:
-          loss = eval(row)
-          losses.append(loss)
-      plt.figure()
-      plt.title('Loss vs. Game')
-      plt.plot(range(len(losses)), losses)
-      plt.xlabel('Game')
-      plt.ylabel('Loss')
-      plt.savefig(LOSSES_PLOT)
-    
+  def compute(self, state):
+    action_probs = self.mcts_search(-state)
+    print(action_probs)
+    return np.argmax(action_probs)
